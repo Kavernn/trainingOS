6c50ab323bf7e584509580011d55fb85782a4ced
Kavernn <kavernn@gmail.com>
2026-03-08 07:43:24 -0400

    Initial Commit

---
+++ TrainingOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainingOS
+//
+//  Created by Vincent Pinard on 2026-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainingOS/TrainingOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingOSApp.swift
+//  TrainingOS
+//
+//  Created by Vincent Pinard on 2026-03-08.
+//
+
+import SwiftUI
+
+@main
+struct TrainingOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
